94ed9fe17a7c8b79a778480f1bac386da44ce4f5
Calise Edeen <caliseedeen@gmail.com>
2026-01-27 16:22:52 -0600

    Fixed robot jitter problem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
@@ -53,10 +54,17 @@ public class RobotContainer {
        drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() -> {
-                double value = Math.min(joystick.getRightTriggerAxis() + 0.25, 1);
-                Voltage outputMultiplier = Volts.of(filter.calculate(value));
+                double outputMultiplier = Math.min(joystick.getRightTriggerAxis() + 0.25, 1);
 
-                return drive.withVelocityX(joystick.getLeftY() * MaxSpeed * outputMultiplier.magnitude()) // Drive forward with negative Y (forward)
+                // SmartDashboard.putNumber("Gas Pedal Value: ", value);
+
+                // Voltage outputMultiplier = Volts.of(filter.calculate(value));
+
+                SmartDashboard.putNumber("Gas Pedal Multipler: ", outputMultiplier);
+
+                SmartDashboard.putNumber("Velocity: ", joystick.getLeftY() * MaxSpeed * outputMultiplier);
+
+                return drive.withVelocityX(joystick.getLeftY() * MaxSpeed * outputMultiplier) // Drive forward with negative Y (forward)
                     .withVelocityY(joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                     .withRotationalRate(-joystick.getRightX() * MaxAngularRate); // Drive counterclockwise with negative X (left)
             })
